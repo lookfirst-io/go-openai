ae5decfc9a74f14743fca47f8aa96e3d198ed590
Pavel Tolstikov <ne2pit@gmail.com>
2025-07-18 19:34:21 +0200

    fix reasoning response

--- chat_stream.go
+++ chat_stream.go
@@ -17,6 +17,8 @@ type ChatCompletionStreamChoiceDelta struct {
 	// the doc from deepseek:
 	// - https://api-docs.deepseek.com/api/create-chat-completion#responses
 	ReasoningContent string `json:"reasoning_content,omitempty"`
+	// Openrouter style reasoning
+	Reasoning string `json:"reasoning,omitempty"`
 }
 
 type ChatCompletionStreamChoiceLogprobs struct {
