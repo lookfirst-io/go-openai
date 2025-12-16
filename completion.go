10ebf16454c632e2d23e39f0edd4c42d7fb5b33c
Manba <adlicabulo@gmail.com>
2025-12-16 11:20:55 +0700

    Revert "feat: add support reasoning effort openai"

--- completion.go
+++ completion.go
@@ -148,13 +148,10 @@ type CompletionRequest struct {
 
 	TopK              int32   `json:"top_k,omitempty"`
 	RepetitionPenalty float64 `json:"repetition_penalty,omitempty"`
-	// Can be "high", "medium", or "low" (OpenAI-style)
-	ReasoningEffort ReasoningEffort `json:"reasoning_effort,omitempty"`
 
 	Provider *PostOpenrouterProviderRequest `json:"provider,omitempty"`
 
-	// Whether to include reasoning in the response for OpenRouter
-	IncludeReasoning bool                            `json:"include_reasoning,omitempty"`
+	IncludeReasoning bool                            `json:"include_reasoning,omitempty"` // Whether to include reasoning in the response for OpenRouter
 	Reasoning        *PostOpenrouterReasoningRequest `json:"reasoning,omitempty"`
 }
 
